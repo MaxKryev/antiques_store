c650ecf0e59435ec1ff1695661b9bf51c28e3496
Максим <kraev-1993@list.ru>
2023-10-25 15:20:10 +0300

    02_created_models

--- my_antiques_store/store/admin.py
+++ my_antiques_store/store/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Goods, User
+from .models import Product, Owner
 
-# Register your models here.
+
+admin.site.register(Owner)
+admin.site.register(Product)
+
--- my_antiques_store/store/models.py
+++ my_antiques_store/store/models.py
@@ -1,25 +1,25 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 
-class Goods(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=100, verbose_name='Товар')
     description = models.CharField(max_length=200, verbose_name='Описание')
     price = models.FloatField(null=True, blank=False, verbose_name='Цена')
-    creation_date = models.DateField(null=True, blank=False, verbose_name='Дата создания')
-    update = models.DateField(null=True, blank=False, verbose_name='Дата обновления')
-    owner = models.CharField(max_length=100, blank=True, verbose_name='Владелец')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    owner = models.ForeignKey('Owner', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
-class User(models.Model):
-    first_name = models.CharField(max_length=100, blank=False, verbose_name='Имя пользователя')
-    last_name = models.CharField(max_length=100, blank=False, verbose_name='Фамилия пользователя')
-    email = models.EmailField(max_length=100, blank=True, verbose_name='Адрес электронной почты')
-    password = models.CharField(max_length=100, blank=False, verbose_name='Пароль')
+class Owner(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name='Имя пользователя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия пользователя')
+    email = models.EmailField(max_length=100, null=True, blank=True, verbose_name='Адрес электронной почты')
 
     def __str__(self):
         return f'{self.last_name} {self.first_name}'
